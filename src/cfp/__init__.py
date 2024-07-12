2a049cbb5976aacc042ed489a87e7d81dfb6432b
joschif <jonas.simon.fleck@gmail.com>
2024-07-12 12:46:24 +0200

    build: import models [skip ci]

--- src/cfp/__init__.py
+++ src/cfp/__init__.py
@@ -1,7 +1,7 @@
 from importlib.metadata import version
 
-from . import data, metrics, networks, training
+from . import data, metrics, networks, training, models
 
-__all__ = ["networks", "metrics", "data", "training"]
+__all__ = ["networks", "metrics", "data", "training", "models"]
 
 __version__ = version("cell_flow_perturbation")
