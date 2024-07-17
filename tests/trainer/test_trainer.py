e7cc410f32fe03dd9439168457505b61152d1e43
joschif <jonas.simon.fleck@gmail.com>
2024-07-17 10:34:22 +0200

    test: use dataloader from conftest

--- tests/trainer/test_trainer.py
+++ tests/trainer/test_trainer.py
@@ -12,23 +12,8 @@ cond = jnp.ones((10, 2, 3))
 vf_rng = jax.random.PRNGKey(111)
 
 
-class DataLoader:
-    n_conditions = 10
-
-    def sample_batch(self, idx, rng):
-        return {
-            "src_lin": jnp.ones((10, 5)) * 10,
-            "tgt_lin": jnp.ones((10, 5)),
-            "src_condition": jnp.ones((10, 2, 3)),
-        }
-
-
-dl = DataLoader()
-dl_list = [dl.sample_batch(i, vf_rng) for i in range(10)]
-
-
 class TestTrainer:
-    def test_otfm(self):
+    def test_otfm(self, dataloader):
         opt = optax.adam(1e-3)
         vf = cfp.networks.ConditionalVelocityField(
             output_dim=5,
@@ -45,13 +30,14 @@ class TestTrainer:
             optimizer=opt,
             rng=vf_rng,
         )
+        dl_list = [dataloader.sample_batch(i, vf_rng) for i in range(10)]
         history = model(dl_list, n_iters=2, rng=vf_rng)
         assert isinstance(history, dict)
 
         x_pred = model.transport(x_test, cond)
         assert x_pred.shape == x_test.shape
 
-    def test_cellflow_trainer(self):
+    def test_cellflow_trainer(self, dataloader):
         opt = optax.adam(1e-3)
         vf = cfp.networks.ConditionalVelocityField(
             output_dim=5,
@@ -69,10 +55,10 @@ class TestTrainer:
             rng=vf_rng,
         )
 
-        trainer = cfp.training.CellFlowTrainer(dataloader=dl, model=model)
+        trainer = cfp.training.CellFlowTrainer(dataloader=dataloader, model=model)
         trainer.train(num_iterations=2, valid_freq=1)
         x_pred = trainer.predict(x_test, cond)
         assert x_pred.shape == x_test.shape
 
-        cond_enc = trainer.encode_conditions(cond)
+        cond_enc = trainer.get_condition_embedding(cond)
         assert cond_enc.shape == (10, 12)
