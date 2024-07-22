303a5e5030d07f767f4fe82e1c93a214bb4dea86
dbobrovskiy <daniilbobrovsky@gmail.com>
2024-07-22 16:35:16 +0200

    feat: custom predict for our format

--- src/cfp/training/trainer.py
+++ src/cfp/training/trainer.py
@@ -1,9 +1,10 @@
 from collections.abc import Callable
 from typing import Any
 
+import diffrax
 import jax
-import numpy as np
 import jax.numpy as jnp
+import numpy as np
 from numpy.typing import ArrayLike
 from ott.neural.methods.flows import genot, otfm
 from ott.solvers import utils as solver_utils
@@ -118,16 +119,16 @@ class CellFlowTrainer:
 
         pbar = tqdm(range(num_iterations))
         for it in pbar:
-            rng, rng_step_fn = jax.random.split(rng, 3)
+            rng, rng_step_fn = jax.random.split(rng, 2)
             batch = dataloader.sample(rng)
 
             src, tgt = batch["src_cell_data"], batch["tgt_cell_data"]
             condition = batch.get("condition")
 
             if isinstance(self.model, genot.GENOT):
-                loss = self._genot_step_fn(rng_step_fn, src, tgt, src_cond)
+                loss = self._genot_step_fn(rng_step_fn, src, tgt, condition)
             else:
-                loss = self._otfm_step_fn(rng_step_fn, src, tgt, src_cond)
+                loss = self._otfm_step_fn(rng_step_fn, src, tgt, condition)
 
             training_logs["loss"].append(float(loss))
             if ((it - 1) % valid_freq == 0) and (it > 1):
@@ -155,7 +156,7 @@ class CellFlowTrainer:
         cond_embed = self.model.vf.apply(
             {"params": self.model.vf_state.params},
             condition,
-            method="encode_conditions",
+            method="get_condition_embedding",
         )
         return np.array(cond_embed)
 
@@ -170,5 +171,28 @@ class CellFlowTrainer:
         -------
             Predicted output
         """
-        x_pred = self.model.transport(x, condition)
+
+        def vf(
+            t: jnp.ndarray, x: jnp.ndarray, cond: dict[str, jnp.ndarray] | None
+        ) -> jnp.ndarray:
+            params = self.model.vf_state.params
+            return self.model.vf_state.apply_fn(
+                {"params": params}, t, x, cond, train=False
+            )
+
+        def solve_ode(x: jnp.ndarray, cond: jnp.ndarray | None) -> jnp.ndarray:
+            ode_term = diffrax.ODETerm(vf)
+            result = diffrax.diffeqsolve(
+                ode_term,
+                t0=0.0,
+                t1=1.0,
+                y0=x,
+                args=cond,
+                dt0=None,
+                solver=diffrax.Tsit5(),
+                stepsize_controller=diffrax.PIDController(rtol=1e-5, atol=1e-5),
+            )
+            return result.ys[0]
+
+        x_pred = jax.jit(jax.vmap(solve_ode, in_axes=[0, None]))(x, condition)
         return np.array(x_pred)
