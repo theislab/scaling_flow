a548f547408c65060dcba5cafa1de72a19d8eb61
joschif <jonas.simon.fleck@gmail.com>
2024-09-11 22:15:36 +0200

    misc: expose more metrics

--- src/cfp/metrics/__init__.py
+++ src/cfp/metrics/__init__.py
@@ -1,3 +1,19 @@
-from cfp.metrics._metrics import compute_mean_metrics, compute_metrics, compute_metrics_fast
+from cfp.metrics._metrics import (
+    compute_mean_metrics,
+    compute_metrics,
+    compute_metrics_fast,
+    compute_scalar_mmd,
+    compute_r_squared,
+    compute_sinkhorn_div,
+    compute_e_distance,
+)
 
-__all__ = ["compute_mean_metrics", "compute_metrics", "compute_metrics_fast"]
+__all__ = [
+    "compute_mean_metrics",
+    "compute_metrics",
+    "compute_metrics_fast",
+    "compute_scalar_mmd",
+    "compute_r_squared",
+    "compute_sinkhorn_div",
+    "compute_e_distance",
+]
