69f00f1d9fbe4d295c049c68d7ba2c298641bb65
guillaumehu <guillaumehuguet2@gmail.com>
2024-11-20 11:46:54 -0500

    fmt

--- src/cfp/preprocessing/_gene_emb.py
+++ src/cfp/preprocessing/_gene_emb.py
@@ -1,5 +1,6 @@
 import requests
-from typing import List
+import scanpy as sc
+
 
 def fetch_canonical_transcript_info(ensembl_gene_id: str):
     server = "https://rest.ensembl.org"
@@ -20,11 +21,12 @@ def fetch_canonical_transcript_info(ensembl_gene_id: str):
         if transcript.get("is_canonical"):
             canonical_transcript_info = {
                 "transcript_id": transcript["id"],
-                "display_name": transcript.get("display_name", "Unknown Protein")
+                "display_name": transcript.get("display_name", "Unknown Protein"),
             }
             break
 
     return canonical_transcript_info
+
 
 def fetch_protein_sequence(ensembl_transcript_id: str):
     server = "https://rest.ensembl.org"
@@ -39,9 +41,10 @@ def fetch_protein_sequence(ensembl_transcript_id: str):
     return protein_data.get("seq", "")
 
 
-def write_sequence_from_ensembl(ensembl_gene_id: List[str], fasta_file_output: str):
+def write_sequence_from_ensembl(ensembl_gene_id: list[str], fasta_file_output: str):
     assert fasta_file_output.endswith(".fasta"), "Output file must be in FASTA format"
     missing_ids = []
+    results = {}
     with open(fasta_file_output, "w") as f:
         for gene_id in ensembl_gene_id:
             canonical_transcript_info = fetch_canonical_transcript_info(gene_id)
@@ -50,10 +53,22 @@ def write_sequence_from_ensembl(ensembl_gene_id: List[str], fasta_file_output: s
                 display_name = canonical_transcript_info["display_name"]
                 protein_sequence = fetch_protein_sequence(transcript_id)
             if protein_sequence:
-                f.write(f">query_{ensembl_gene_id}_canonical_transcript_{transcript_id}_Name_{display_name}\n")
+                results[gene_id] = protein_sequence
+                f.write(
+                    f">query_{ensembl_gene_id}_canonical_transcript_{transcript_id}_Name_{display_name}\n"
+                )
                 f.write(f"{protein_sequence}\n")
             else:
                 missing_ids.append(gene_id)
-    print(f"Missing sequence for ids: {set(missing_ids)}")     
-    return fasta_file_output       
+    print(f"Missing sequence for ids: {set(missing_ids)}")
+    return results
 
+
+def augment_adata_with_gene_embeddings(
+    adata: sc.AnnData, treat_key: str = "perturbation"
+) -> sc.AnnData:
+    gene_ids = adata.obs[treat_key].unique().tolist()
+    res = write_sequence_from_ensembl(gene_ids, "gene_embeddings.fasta")
+    # TODO: not sure how / where to add them.
+    adata.obs["gene_embedding"] = res
+    pass
