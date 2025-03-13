19987e24f93ebab25ed66a923fc23e7c707b3041
Dominik Klein <dominik.klein@helmholtz-munich.de>
2025-03-13 10:55:42 +0100

    adapt toml

--- src/cfp/__init__.py
+++ src/cfp/__init__.py
@@ -4,14 +4,3 @@ from cfp import data, datasets, external, metrics, model, networks, solvers, tra
 from cfp import preprocessing as pp
 
 __version__ = metadata.version("cell_flow_perturbation")
-try:
-    md = metadata.metadata(__name__)
-    __version__ = md.get("version", "")  # type: ignore[attr-defined]
-    __author__ = md.get("Author", "")  # type: ignore[attr-defined]
-    __maintainer__ = md.get("Maintainer-email", "")  # type: ignore[attr-defined]
-except ImportError:
-    md = None
-    __version__ = "0.0.1"
-    __author__ = "TODO"
-
-del metadata, md
