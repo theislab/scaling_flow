58f62447ee3475a0371df5d8f1794b5b22118f9f
LeonStadelmann <leonstadlmnn@gmail.com>
2025-04-07 21:54:22 +0200

    Improve metrics documentation

--- src/cellflow/metrics/_metrics.py
+++ src/cellflow/metrics/_metrics.py
@@ -6,6 +6,7 @@ from ott.geometry import costs, pointcloud
 from ott.tools.sinkhorn_divergence import sinkhorn_divergence
 from sklearn.metrics import pairwise_distances, r2_score
 from sklearn.metrics.pairwise import rbf_kernel
+from collections.abc  import Sequence
 
 __all__ = [
     "compute_metrics",
@@ -21,12 +22,36 @@ __all__ = [
 
 
 def compute_r_squared(x: ArrayLike, y: ArrayLike) -> float:
-    """Compute the R squared between true (x) and predicted (y)"""
+    """Compute the R squared score between the true (x) and predicted (y) distributions.
+
+    Parameters
+    ----------
+        x
+            An array of shape [num_samples, num_features].
+        y
+            An array of shape [num_samples, num_features].
+    Returns
+    -------
+        A scalar denoting the R squared score.
+    """
     return r2_score(np.mean(x, axis=0), np.mean(y, axis=0))
 
 
 def compute_sinkhorn_div(x: ArrayLike, y: ArrayLike, epsilon: float = 1e-2) -> float:
-    """Compute the Sinkhorn divergence between x and y."""
+    """Compute the Sinkhorn divergence between x and y.
+
+    Parameters
+    ----------
+        x
+            An array of shape [num_samples, num_features].
+        y
+            An array of shape [num_samples, num_features].
+        epsilon
+            The regularization parameter.
+    Returns
+    -------
+        A scalar denoting the sinkhorn divergence value.
+    """
     return float(
         sinkhorn_divergence(
             pointcloud.PointCloud,
@@ -35,12 +60,23 @@ def compute_sinkhorn_div(x: ArrayLike, y: ArrayLike, epsilon: float = 1e-2) -> f
             cost_fn=costs.SqEuclidean(),
             epsilon=epsilon,
             scale_cost=1.0,
-        ).divergence[0]
+        )[0]
     )
 
 
 def compute_e_distance(x: ArrayLike, y: ArrayLike) -> float:
-    """Compute the energy distance as in Peidli et al."""
+    """Compute the energy distance between x and y as in Peidli et al.
+
+    Parameters
+    ----------
+        x
+            An array of shape [num_samples, num_features].
+        y
+            An array of shape [num_samples, num_features].
+    Returns
+    -------
+        A scalar denoting the energy distance value.
+    """
     sigma_X = pairwise_distances(x, x, metric="sqeuclidean").mean()
     sigma_Y = pairwise_distances(y, y, metric="sqeuclidean").mean()
     delta = pairwise_distances(x, y, metric="sqeuclidean").mean()
@@ -54,7 +90,18 @@ def pairwise_squeuclidean(x: ArrayLike, y: ArrayLike) -> ArrayLike:
 
 @jax.jit
 def compute_e_distance_fast(x: ArrayLike, y: ArrayLike) -> float:
-    """Compute the energy distance as in Peidli et al."""
+    """Compute the energy distance between x and y as in Peidli et al.
+
+    Parameters
+    ----------
+        x
+            An array of shape [num_samples, num_features].
+        y
+            An array of shape [num_samples, num_features].
+    Returns
+    -------
+        A scalar denoting the energy distance value.
+    """
     sigma_X = pairwise_squeuclidean(x, x).mean()
     sigma_Y = pairwise_squeuclidean(y, y).mean()
     delta = pairwise_squeuclidean(x, y).mean()
@@ -62,7 +109,25 @@ def compute_e_distance_fast(x: ArrayLike, y: ArrayLike) -> float:
 
 
 def compute_metrics(x: ArrayLike, y: ArrayLike) -> dict[str, float]:
-    """Compute different metrics for x (true) and y (predicted)."""
+    """Compute a set of metrics between two distributions x and y.
+
+    Parameters
+    ----------
+        x
+            An array of shape [num_samples, num_features].
+        y
+            An array of shape [num_samples, num_features].
+    Returns
+    -------
+        A dictionary containing the following computed metrics: 
+        
+        - the r squared score.
+        - the sinkhorn divergence with ``epsilon`` = 1.0.
+        - the sinkhorn divergence with ``epsilon`` = 10.0.
+        - the sinkhorn divergence with ``epsilon`` = 100.0.
+        - the energy distance value.
+        - the mean maximum discrepancy loss
+    """
     metrics = {}
     metrics["r_squared"] = compute_r_squared(x, y)
     metrics["sinkhorn_div_1"] = compute_sinkhorn_div(x, y, epsilon=1.0)
@@ -74,7 +139,20 @@ def compute_metrics(x: ArrayLike, y: ArrayLike) -> dict[str, float]:
 
 
 def compute_mean_metrics(metrics: dict[str, dict[str, float]], prefix: str = "") -> dict[str, list[float]]:
-    """Compute the mean value of different metrics."""
+    """Compute the mean value of different metrics.
+    
+    Parameters
+    ----------
+        metrics
+            A dictionary where the keys indicate the name of the pertubations and the values are
+            dictionaries containing computed metrics.
+        prefix
+            A string definining the prefix of all metrics in the output dictionary.
+    Returns
+    -------
+        A dictionary where the keys indicate the metrics and the values contain the average metric
+        values over all pertubations.
+    """
     metric_names = list(list(metrics.values())[0].keys())
     metric_dict: dict[str, list[float]] = {prefix + met_name: [] for met_name in metric_names}
     for met in metric_names:
@@ -95,16 +173,22 @@ def rbf_kernel_fast(x: ArrayLike, y: ArrayLike, gamma: float) -> ArrayLike:
 
 
 def maximum_mean_discrepancy(x: ArrayLike, y: ArrayLike, gamma: float = 1.0, exact: bool = False) -> float:
-    """Compute the Maximum Mean Discrepancy (MMD) between two samples: x and y.
+    """Compute the Maximum Mean Discrepancy (MMD) between two distributions x and y.
 
-    Args:
-        x: a tensor of shape [num_samples, num_features]
-        y: a tensor of shape [num_samples, num_features]
-        exact: a bool
+    Parameters
+    ----------
+        x
+            An array of shape [num_samples, num_features].
+        y
+            An array of shape [num_samples, num_features].
+        gamma
+            Parameter for the rbf kernel.
+        exact
+            Use exact or fast rbf kernel.
 
     Returns
     -------
-        a scalar denoting the squared maximum mean discrepancy loss.
+        A scalar denoting the squared maximum mean discrepancy loss.
     """
     kernel = rbf_kernel if exact else rbf_kernel_fast
     xx = kernel(x, x, gamma)
@@ -113,8 +197,22 @@ def maximum_mean_discrepancy(x: ArrayLike, y: ArrayLike, gamma: float = 1.0, exa
     return xx.mean() + yy.mean() - 2 * xy.mean()
 
 
-def compute_scalar_mmd(x: ArrayLike, y: ArrayLike, gammas: float | None = None) -> float:
-    """Compute MMD across different length scales"""
+def compute_scalar_mmd(x: ArrayLike, y: ArrayLike, gammas: Sequence[float] | None = None) -> float:
+    """Compute the Mean Maximum Discrepancy (MMD) across different length scales
+
+    Parameters
+    ----------
+        x
+            An array of shape [num_samples, num_features].
+        y
+            An array of shape [num_samples, num_features].
+        gammas
+            A sequence of values for the paramater gamma of the rbf kernel.
+    Returns
+    -------
+        A scalar denoting the average MMD over all gammas.
+    """
+
     if gammas is None:
         gammas = [2, 1, 0.5, 0.1, 0.01, 0.005]
     mmds = [maximum_mean_discrepancy(x, y, gamma=gamma) for gamma in gammas]  # type: ignore[union-attr]
@@ -122,7 +220,24 @@ def compute_scalar_mmd(x: ArrayLike, y: ArrayLike, gammas: float | None = None) 
 
 
 def compute_metrics_fast(x: ArrayLike, y: ArrayLike) -> dict[str, float]:
-    """Compute metrics which are fast to compute."""
+    """Compute metrics which are fast to compute
+
+    Parameters
+    ----------
+        x
+            An array of shape [num_samples, num_features].
+        y
+            An array of shape [num_samples, num_features].
+
+    Returns
+    -------
+        A dictionary containing the following computed metrics: 
+        
+        - the r squared score.
+        - the energy distance value.
+        - the mean maximum discrepancy loss
+    """
+
     metrics = {}
     metrics["r_squared"] = compute_r_squared(x, y)
     metrics["e_distance"] = compute_e_distance(x, y)
