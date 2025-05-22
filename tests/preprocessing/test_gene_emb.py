7840c80034a53184c245a8939bde077c89e713c2
Dominik Klein <dominik.klein@helmholtz-munich.de>
2025-05-22 13:16:42 +0200

    skip gene emb tests

--- tests/preprocessing/test_gene_emb.py
+++ tests/preprocessing/test_gene_emb.py
@@ -52,13 +52,15 @@ def adata_test_legacy():
 
 
 class TestGeneEmb:
+    @pytest.skip(reason="temporarily disabled")
     def test_embedding(self, adata_with_ko):
         adata = get_esm_embedding(adata_with_ko, gene_key="gene_target_", copy=True)
         metadata = adata.uns["gene_embedding_metadata"]
         assert Counter(metadata[metadata.is_protein_coding].gene_id.tolist()) == IS_PROT_CODING
         gene_with_prot_seq = metadata[metadata.protein_sequence.notnull()].gene_id.tolist()
         assert Counter(gene_with_prot_seq) == IS_PROT_CODING
-
+        
+    @pytest.skip(reason="temporarily disabled")
     def test_legacy_emb(self, adata_test_legacy):
         """Test if we can reproduce the original embeddings we used."""
         adata = get_esm_embedding(adata_test_legacy, gene_key="gene", copy=True)
